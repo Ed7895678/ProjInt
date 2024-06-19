9f092e5b0768027623f050d6d523f573f983ac37
edSantos <profedacc@gmail.com>
2024-06-19 20:23:26 +0100

    commit

--- js/logic.js
+++ js/logic.js
@@ -18,99 +18,85 @@ function calcularIMI() {
 
 
 // Função para calcular o IMT
+// Função para calcular o IMT
 function calcularIMT() {
-    // Input
-    let valorImovel = parseFloat(document.getElementById("valorImovel").value);
-    let tipoHabitacao = document.getElementById("tipoHabitacao").value;
-    let localizacao = document.getElementById("localizacao").value;
-
-    let fatorRegiaoAutonoma = 0.875;
-    let imt;
-
-    console.log(valorImovel)
-    console.log(tipoHabitacao)
-    console.log(localizacao)
+    const precoImovel = parseFloat(document.getElementById('precoImovel').value);
+    const finalidadeImovel = document.getElementById('finalidadeImovel').value;
+    const localImovel = document.getElementById('localImovel').value;
+    let imt = 0;
 
     // Validação
-    if (isNaN(valorImovel)) {
-        document.getElementById("error").innerHTML = "Preencha todos os campos de input."
-        document.getElementById("output").innerText = "";
+    if (isNaN(precoImovel)) {
+        document.getElementById("error").innerHTML = "Preencha todos os campos de input.";
+        document.getElementById("output").innerHTML = "";
+        return;
     } else {
-        // Tabelas e Calculos
-        if (tipoHabitacao == "propria_permanente") {
-            if (valorImovel <= 97064) {
-                imt = 0;
-            } else if (valorImovel <= 132774) {
-                imt = (valorImovel - 97064) * 0.02 - 1941.28;
-            } else if (valorImovel <= 181034) {
-                imt = (valorImovel - 132774) * 0.05 - 5895.64;
-            } else if (valorImovel <= 301688) {
-                imt = (valorImovel - 181034) * 0.07 - 9287.56;
-            } else if (valorImovel <= 603274) {
-                imt = (valorImovel - 301688) * 0.08 - 12785.88;
-            } else {
-                imt = valorImovel * 0.06;
-            }
-        } else if (tipoHabitacao == "secundaria") {
-            if (valorImovel <= 97064) {
-                imt = valorImovel * 0.01;
-            } else if (valorImovel <= 132774) {
-                imt = (valorImovel - 97064) * 0.02 + 970.64;
-            } else if (valorImovel <= 181034) {
-                imt = (valorImovel - 132774) * 0.05 - 3941.28;
-            } else if (valorImovel <= 301688) {
-                imt = (valorImovel - 181034) * 0.07 - 9287.56;
-            } else if (valorImovel <= 603274) {
-                imt = (valorImovel - 301688) * 0.08 - 12785.88;
-            } else {
-                imt = valorImovel * 0.06;
-            }
+        document.getElementById("error").innerHTML = "";
+    }
+
+    if(localImovel == 'portugal') {
+        if (precoImovel <= 101917) {
+            imt = 0;
+        } else if (precoImovel <= 139412) {
+            imt = precoImovel * 0.02 - 2038.34;
+        } else if (precoImovel <= 190086) {
+            imt = precoImovel * 0.05 - 6220.70;
+        } else if (precoImovel <= 316772) {
+            imt = precoImovel * 0.07 - 10022.42;
+        } else if (precoImovel <= 633453) {
+            imt = precoImovel * 0.08 - 13190.14;
+        } else if (precoImovel <= 1102920) {
+            imt = precoImovel * 0.06;
+        } else {
+            imt = precoImovel * 0.075;
         }
-    }   
-    if (localizacao == "regiao_autonoma") {
-        imt = imt * fatorRegiaoAutonoma;
+    }else if (localImovel == 'regioes'){
+        if (precoImovel <= 127396.25) {
+            imt = 0;
+        } else if (precoImovel <= 174265.00) {
+            imt = precoImovel * 0.02 - 2547.93;
+        } else if (precoImovel <= 237607.50) {
+            imt = precoImovel * 0.05 - 7775.88;
+        } else if (precoImovel <= 395965.00) {
+            imt = precoImovel * 0.07 - 12528.03;
+        } else if (precoImovel <= 791816.25) {
+            imt = precoImovel * 0.08 - 16487.67;
+        } else if (precoImovel <= 1378650.00) {
+            imt = precoImovel * 0.06;
+        } else {
+            imt = precoImovel * 0.075;
+        }
     }
-    if (imt > 0){
-        document.getElementById("output").innerHTML = imt.toFixed(2) + "€"
-    } else {
-        document.getElementById("output").innerHTML = "Isento"
+
+    if (finalidadeImovel == 'secundaria') {
+        imt += 1000;
     }
-} 
+
+    const impostoSelo = precoImovel * 0.008;
+    const total = imt + impostoSelo;
+
+    document.getElementById('output').innerHTML = total.toFixed(2) + "€"
+}
 
 
-
-
-// Função para calcular a Taxa Euribor
-function calcularEuribor() {
+// Função para calcular Prestação ao Credito Habitação
+function calcularPrestacao() {
     // Input
-    let principal = parseFloat(document.getElementById('principal').value);
-    let euribor = parseFloat(document.getElementById('euribor').value);
-    let duration = parseInt(document.getElementById('duration').value);
-
-    const euriborRates = {
-        '1': 3.816,
-        '3': 3.813,
-        '6': 3.787
-    }
-
-    if (isNaN(principal) || isNaN(euribor) || isNaN(duration)) {
+    let divida = parseFloat(document.getElementById("divida").value);
+    let juro = parseFloat(document.getElementById("juro").value);
+    let anos = parseFloat(document.getElementById("anos").value);
+    // Validação 
+    if(isNaN(divida) || isNaN(juro) || isNaN(anos)){
         document.getElementById("error").innerHTML = "Preencha todos os campos de input."
         document.getElementById("output").innerText = "";
     } else {
         document.getElementById("error").innerHTML = ""
-        duration /= 30
-        
-        if (duration <= 1) {
-            adjustedEuribor = euriborRates['1'];
-        } else if (duration <= 3) {
-            adjustedEuribor = euriborRates['3'];
-        } else {
-            adjustedEuribor = euriborRates['6'];
-        }
-    
-        let monthlyRate = adjustedEuribor / 100 / 12;
-        let monthlyPayment = (principal * monthlyRate) / (1 - Math.pow(1 + monthlyRate, - duration));
-        document.getElementById('output').innerHTML = monthlyPayment.toFixed(2) + "; " + adjustedEuribor;
+    // Calculo
+        let juroMensal = (juro/100) / 12;
+        let numMensal = anos * 12;
+
+        let output = (divida * juroMensal * Math.pow(1 + juroMensal, numMensal)) / (Math.pow(1 + juroMensal, numMensal) - 1)
+        document.getElementById("output").innerText = output.toFixed(2) + "€";  
     }
 }
 
@@ -180,34 +166,33 @@ function calcularSalario() {
 
 // Função para calcular a Pensao Liquida
 function calcularPensao() {
-    let impostoRenda = 0;
-    let contribuicaoPrevidenciaria = 0;
-    let outrasDeducoes = 0;
+    // Obter valores dos campos de input
+    const pensaoBruta = parseFloat(document.getElementById('pensaoBruta').value);
+    const localizacao = document.getElementById('localizacao').value;
+    const parcelaAbater = parseFloat(document.getElementById('parcelaAbater').value);
 
-    let pensaoBruta = document.getElementById("pensaoBruta").value;
-    let localizacao = document.getElementById("localizacao").value;
-    let dependentes = document.getElementById("dependentes").value;
-    let dependentesDeficiencia = document.getElementById("dependentesDeficiencia").value;
-    let parcelaAbater = document.getElementById("parcelaAbater").value;
-
-    // Validação 
-    if(isNaN(pensaoBruta) || localizacao == "" || isNaN(dependentes) || isNaN(dependentesDeficiencia) || isNaN(parcelaAbater)){
+    if(isNaN(pensaoBruta) || localizacao == "" || isNaN(parcelaAbater)){
         document.getElementById("error").innerHTML = "Preencha todos os campos de input."
         document.getElementById("output").innerText = "";
     } else {
+        document.getElementById("error").innerHTML = ""
+    // Calculo
+    let impostoRenda = 0;
+    let contribuicaoPrevidenciaria = pensaoBruta * 0.11;
 
-        if (localizacao === 'continental') {
-            impostoRenda = pensaoBruta * 0.15 - parcelaAbater;
-        } else {
-            impostoRenda = pensaoBruta * 0.12 - parcelaAbater;
-        }
-        contribuicaoPrevidenciaria = pensaoBruta * 0.11;
-        outrasDeducoes = dependentes * 50 + dependentesDeficiencia * 100;
-    
-        let output = (pensaoBruta - (impostoRenda + contribuicaoPrevidenciaria + outrasDeducoes));
-        document.getElementById("output").innerHTML = output + "€";
+    if (localizacao == 'continental') {
+        impostoRenda = pensaoBruta * 0.15;
+    } else {
+        impostoRenda = pensaoBruta * 0.12;
+    }
+    impostoRenda -= parcelaAbater;
+
+    const pensaoLiquida = pensaoBruta - (impostoRenda + contribuicaoPrevidenciaria);
+    document.getElementById('output').innerText = pensaoLiquida.toFixed(2) + "€";
     }
 }
+
+
 
 
 // Função para calcular Juros Compostos
@@ -226,4 +211,13 @@ function calcularJuros() {
 
         document.getElementById("output").innerText = output + "€";
     }
+}
+
+function submeterEmail() {
+    let assunto = document.getElementById("assunto").value
+    let mensagem = document.getElementById("mensagem").value
+
+    console.log(assunto, mensagem)
+
+    alert("Assunto: " + assunto + "\nMensagem: " + mensagem)
 }
